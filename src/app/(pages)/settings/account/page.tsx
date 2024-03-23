3418549e174bc41db0c714933bc7689ebd87544b
tomato-ga <servicedake@gmail.com>
2024-03-23 11:16:09 +0900

    ログインまわりスタート

---
+++ src/app/(pages)/settings/account/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Account = () => {
+	return <div>Account</div>
+}
+
+export default Account
+
+// TODO supabase login auth
+// MEMO https://note.com/libproc/n/n168e87864291
+// MEMO https://supabase.com/docs/guides/auth/auth-helpers/nextjs
+// MEMO https://supabase.com/docs/guides/auth/server-side/nextjs
