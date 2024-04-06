efef273e433933e33cf6b4adeadf8cd1265c7ffe
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-04-06 23:17:44 +0900

    投稿からDB保存はできた

--- src/app/api/db/savePost/route.ts
+++ src/app/api/db/savePost/route.ts
@@ -15,24 +15,46 @@ export async function POST(request: Request) {
 		const postData: postDBinsert = await request.json()
 		console.log('postData', postData)
 
+		// メールアドレスからユーザーを検索
+		const user = await prisma.user.findUnique({
+			where: { email: postData.id }
+		})
+
+		if (!user) {
+			throw new Error('User not found')
+		}
+
+		console.log('userID:', user.id)
+
 		const createdPost = await prisma.post.create({
 			data: {
-				userId: postData.id,
+				userId: user.id,
 				title: postData.title,
 				description: postData.description,
 				imageUrl: postData.imageurl || null,
 				videoUrl: postData.videourl || null,
 				tags: {
-					connectOrCreate:
+					create:
 						postData.tags?.map((tagName) => ({
-							where: { name: tagName },
-							create: { name: tagName }
+							tag: {
+								connectOrCreate: {
+									where: { name: tagName },
+									create: { name: tagName }
+								}
+							}
 						})) || []
+				}
+			},
+			include: {
+				tags: {
+					include: {
+						tag: true
+					}
 				}
 			}
 		})
 
-		console.log('Post created:', createdPost)
+		console.log('Post created:', JSON.stringify(createdPost, null, 2))
 		return NextResponse.json({ message: 'Post created successfully' })
 	} catch (error) {
 		console.error('Error creating post:', error)
