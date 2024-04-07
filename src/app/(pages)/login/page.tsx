9cd494d504d29a5e24b374c71df263bfd43b2a72
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-04-08 00:00:33 +0900

    TODO : プロフィール構築

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,18 +1,14 @@
+// LoginPage.tsx
 'use client'
-
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from '@/app/components/Login'
 import Logout from '@/app/components/Logout'
 import { useSession } from 'next-auth/react'
 import { insertUserData } from '@/app/func/dbUserInsert'
-import { checkUserExists } from '@/app/func/checkUserExists'
-
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/auth/[...nextauth]'
 
 const LoginPage = () => {
-	const { data: session, status } = useSession() // クライアントコンポーネントのSession情報取得
-
+	const { data: session, status } = useSession()
+	const [userExists, setUserExists] = useState(false)
 
 	useEffect(() => {
 		const insertUser = async () => {
@@ -20,18 +16,17 @@ const LoginPage = () => {
 				const name = session.user?.name || ''
 				const email = session.user?.email || ''
 				const image = session.user?.image || ''
-
 				if (email) {
 					try {
-						await insertUserData(name, email, image)
+						const result = await insertUserData(name, email, image)
+						setUserExists(result.userExists)
 					} catch (error) {
 						console.error('データ挿入時のエラー:', error)
-						// 必要に応じてエラーメッセージを表示するなどの処理を行う
+						// エラーメッセージを表示するなどの適切な処理を行う
 					}
 				}
 			}
 		}
-
 		if (status === 'authenticated') {
 			insertUser()
 		}
@@ -44,6 +39,7 @@ const LoginPage = () => {
 					<p>セッションの期限：{session.expires}</p>
 					<p>ようこそ、{session.user?.name}さん</p>
 					<img src={session.user?.image ?? ''} alt="" style={{ borderRadius: '50px' }} />
+					{userExists && <p>ユーザーは既に存在します</p>}
 					<div>
 						<Logout />
 					</div>
--- src/app/func/dbUserInsert/index.ts
+++ src/app/func/dbUserInsert/index.ts
@@ -1,4 +1,5 @@
-export async function insertUserData(name: string, email: string, image?: string): Promise<void> {
+// dbUserInsert.ts
+export async function insertUserData(name: string, email: string, image?: string): Promise<{ userExists: boolean }> {
 	try {
 		const response = await fetch('http://localhost:3000/api/db/userSave', {
 			method: 'POST',
@@ -7,16 +8,17 @@ export async function insertUserData(name: string, email: string, image?: string
 			},
 			body: JSON.stringify({ name, email, image })
 		})
-
 		if (response.ok) {
 			const data = await response.json()
-			console.log(data.message) // レスポンスのメッセージをコンソールに出力
+			console.log(data.message)
+			return { userExists: false }
 		} else if (response.status === 400) {
-			console.log('ユーザーは既に存在します')
+			return { userExists: true }
 		} else {
 			throw new Error('Failed to insert user data')
 		}
 	} catch (error) {
+		console.error('データ挿入時のエラー:', error)
 		throw error
 	}
 }
