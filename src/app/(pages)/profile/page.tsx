5a67146ea40d9ccbbb0894b0c57231032a8c8944
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-04-08 00:00:33 +0900

    TODO : プロフィール構築

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,17 +1,21 @@
+// src/app/(pages)/profile/page.tsx
+
 import { getServerSession } from 'next-auth/next'
 import { authOptions } from '@/auth/[...nextauth]'
 import { prisma } from '@/app/lib/prisma'
 import ProfilePanel from '@/app/components/ProfilePanel'
 
-interface Props {
-	params: { screenName: string }
-}
+export default async function ProfilePage() {
+	const session = await getServerSession(authOptions)
+	if (!session || !session.user || !session.user.name) {
+		return <div>ユーザーセッションが見つかりません</div>
+	}
+	const screenName = session.user.name
 
-export default async function Page({ params }: Props) {
 	// 【1】params.screenNameから、プロフィールを取得
 	const profile = await prisma.profile.findUnique({
 		where: {
-			screenName: params.screenName
+			screenName: screenName
 		},
 		include: {
 			user: true
@@ -19,30 +23,17 @@ export default async function Page({ params }: Props) {
 	})
 
 	if (!profile) {
-		// プロフィールが見つからない場合の処理
-		return <>プロフィールが見つかりません</>
+		// プロフィールが存在しない場合の処理
+		return <div>Profile not found</div>
 	}
 
-	// 【2】特定したユーザーのIDで、投稿一覧を取得
-	const photos = await prisma.post.findMany({
-		where: {
-			userId: profile.userId
-		},
-		include: {
-			tags: true
-		},
-		take: 15 // 最大15件取得
-	})
-
+	// 【2】プロフィールを表示
 	return (
-		<>
-			プロフィールパネル予定
-			{/* <ProfilePanel
-				// imageUrl={profile.user.image}
-				name={profile.user.name || ''}
-				screenName={profile.screenName || ''}
-				bio={profile.bio || ''}
-			/> */}
-		</>
+		<div>
+			<h1>{profile.screenName}</h1>
+			<p>{profile.bio}</p>
+			{/* 他のプロフィール情報を表示 */}
+			{/* TODO プロフィールコンポーネント作成する */}
+		</div>
 	)
 }
