e380cbc485591fb011ff90c964552ec2f693a675
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-04-15 16:38:44 +0900

    動画のみアップロードに変更

--- src/app/components/Upload/PreviewSection.tsx
+++ src/app/components/Upload/PreviewSection.tsx
@@ -3,11 +3,11 @@ import { CircularProgress } from '@mui/material'
 
 interface PreviewSectionProps {
 	videoUrl?: string
-	imageUrls: string[]
+	// imageUrls: string[]
 	isLoading: boolean
 }
 
-const PreviewSection: React.FC<PreviewSectionProps> = ({ videoUrl, imageUrls, isLoading }) => {
+const PreviewSection: React.FC<PreviewSectionProps> = ({ videoUrl, isLoading }) => {
 	return (
 		<div className="mt-8">
 			<h2 className="text-2xl font-semibold mb-4 text-center">アップロードしたファイルのプレビュー</h2>
@@ -16,9 +16,9 @@ const PreviewSection: React.FC<PreviewSectionProps> = ({ videoUrl, imageUrls, is
 					<video controls src={videoUrl} className="max-w-full mx-auto mb-4" />
 				</>
 			)}
-			{imageUrls.map((imageUrl, index) => (
+			{/* 2024/04/15 画像アップロードはしないように変更 {imageUrls.map((imageUrl, index) => (
 				<img key={index} src={imageUrl} alt={`Uploaded Image ${index + 1}`} className="max-w-full mx-auto mb-4" />
-			))}
+			))} */}
 
 			{isLoading && (
 				<div className="flex justify-center items-center mt-4">
--- src/app/components/Upload/SaveButton.tsx
+++ src/app/components/Upload/SaveButton.tsx
@@ -8,7 +8,7 @@ const SaveButton: React.FC<SaveButtonProps> = ({ onSave }) => {
 	return (
 		<div className="text-center">
 			<button className="bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded" onClick={onSave}>
-				保存する
+				投稿する
 			</button>
 		</div>
 	)
