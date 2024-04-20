a93415b5bf9139d3beee7e50fd12f2e7732499a3
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-04-20 17:16:12 +0900

    動画初期設定muteにした

--- src/app/(pages)/post/[postId]/page.tsx
+++ src/app/(pages)/post/[postId]/page.tsx
@@ -1,27 +1,40 @@
 // /Users/donbe/Codes/keysound/src/app/(pages)/post/[postId]/page.tsx
 
-import React from "react";
-import { prisma } from "@/app/lib/prisma";
-import { formatDate } from "@/app/func/postFunc";
-import dynamic from "next/dynamic";
+import React from 'react'
+import { prisma } from '@/app/lib/prisma'
+import { formatDate } from '@/app/func/postFunc'
+import dynamic from 'next/dynamic'
 
 const DynamicVideoPlayer = dynamic<{ videoUrl: string; controls: boolean }>(
-	() => import("../../../components/VideoPlayer"),
-	{ ssr: false },
-);
+	() => import('../../../components/VideoPlayer'),
+	{ ssr: false }
+)
 
 interface PostPageProps {
-	params: { postId: string };
+	readonly params: { postId: string }
 }
 
 const PostPage = async ({ params }: PostPageProps) => {
 	const post = await prisma.post.findUnique({
 		where: { id: params.postId },
-		include: { user: true },
-	});
+		include: {
+			user: true,
+			tags: {
+				include: {
+					tag: true
+				}
+			},
+			part: true
+		}
+	})
+
+	// タグを配列にして格納
+	const tagsArray = post?.tags.map((tagItem) => tagItem.tag.name)
+
+	// パーツを配列にして格納
 
 	if (!post) {
-		return <div>Post not found</div>;
+		return <div>Post not found</div>
 	}
 
 	return (
@@ -33,41 +46,62 @@ const PostPage = async ({ params }: PostPageProps) => {
 							<DynamicVideoPlayer videoUrl={post.videoUrl} controls={true} />
 						) : (
 							// <video src={post.videoUrl} controls className="w-full h-96 object-cover" />
-							<img
-								src={post.imageUrl || "/default-image.jpg"}
-								alt={post.title}
-								className="w-full h-96 object-cover"
-							/>
+							<img src={post.imageUrl || '/default-image.jpg'} alt={post.title} className="w-full h-96 object-cover" />
 						)}
 					</div>
 					<div className="p-8">
 						<div className="flex items-center mb-4">
 							<img
-								src={post.user.image || "/default-avatar.jpg"}
+								src={post.user.image || '/default-avatar.jpg'}
 								alt={post.user.name}
 								className="w-12 h-12 rounded-full mr-4"
 							/>
 							<div>
-								<p className="text-gray-900 font-semibold text-lg">
-									{post.user.name}
-								</p>
-								<p className="text-gray-600 text-sm">
-									{formatDate(post.createdat)}
-								</p>
+								<p className="text-gray-900 font-semibold text-lg">{post.user.name}</p>
+								<p className="text-gray-600 text-sm">{formatDate(post.createdat)}</p>
 							</div>
 						</div>
-						<h1 className="text-gray-600 text-2xl font-bold py-4">
-							{post.title}
-						</h1>
-						<p className="text-gray-600  leading-relaxed mb-8 py-4 whitespace-pre-wrap">
-							{post.description}
-						</p>
+
+						<h1 className="text-gray-600 text-4xl font-bold py-4">{post.title}</h1>
+						<div className="tag">
+							{tagsArray?.map((tag, index) => (
+								<div key={index} className="text-gray-600 bg-blue-50 rounded-md px-4 py-2 mr-2 mb-2 inline-block">
+									#{tag}
+								</div>
+							))}
+						</div>
+
+						<h2 className="text-gray-600 font-bold py-4 text-2xl">パーツ</h2>
+						<div className="bg-white rounded-lg py-2">
+							{post.part && (
+								<ul className="space-y-4">
+									<li className="flex items-center">
+										<span className="font-semibold w-32">ケース:</span>
+										<span>{post.part.case}</span>
+									</li>
+									<li className="flex items-center">
+										<span className="font-semibold w-32">プレート:</span>
+										<span>{post.part.plate}</span>
+									</li>
+									<li className="flex items-center">
+										<span className="font-semibold w-32">スイッチ:</span>
+										<span>{post.part.switches}</span>
+									</li>
+									<li className="flex items-center">
+										<span className="font-semibold w-32">キーキャップ:</span>
+										<span>{post.part.keyCaps}</span>
+									</li>
+								</ul>
+							)}
+						</div>
+
+						<p className="text-gray-600  leading-relaxed mb-8 py-4 whitespace-pre-wrap">{post.description}</p>
 						{/* Add any additional post details or components here */}
 					</div>
 				</div>
 			</div>
 		</div>
-	);
-};
+	)
+}
 
-export default PostPage;
+export default PostPage
--- src/app/components/VideoPlayer/index.tsx
+++ src/app/components/VideoPlayer/index.tsx
@@ -1,23 +1,20 @@
 /**
- * TypeScript React の `DynamicVideoPlayer` コンポーネントは、動的な URL とレスポンシブなスタイルを備えたビデオ プレーヤーをレンダリングします。
- * @param  - `DynamicVideoPlayer.tsx` ファイルは、`react-player` ライブラリの `ReactPlayer` コンポーネントを使用してビデオ
- * プレーヤーをレンダリングする TypeScript React コンポーネントです。
+ * TypeScript React の `DynamicVideoPlayer` コンポーネントは、`react-player` ライブラリを使用して、動的 URL
+ * とレスポンシブなスタイルを備えたビデオ プレーヤーをレンダリングします。
+ * @param  - 提供されている TypeScript React コードの `DynamicVideoPlayer` コンポーネントは、動的な URL とレスポンシブなスタイルを使用してビデオ
+ * プレーヤーをレンダリングします。
  */
 
 // /Users/ore/Documents/GitHub/keysound/src/app/components/VideoPlayer/index.tsx
-"use client";
-import { FC } from "react";
-import ReactPlayer from "react-player/lazy";
+'use client'
+import ReactPlayer from 'react-player/lazy'
 
 interface DynamicVideoPlayerProps<T extends boolean> {
-	videoUrl: string;
-	controls?: T;
+	videoUrl: string
+	controls?: T
 }
 
-const DynamicVideoPlayer = <T extends boolean = false>({
-	videoUrl,
-	controls,
-}: DynamicVideoPlayerProps<T>) => {
+const DynamicVideoPlayer = <T extends boolean = false>({ videoUrl, controls }: DynamicVideoPlayerProps<T>) => {
 	return (
 		<ReactPlayer
 			url={videoUrl}
@@ -25,8 +22,11 @@ const DynamicVideoPlayer = <T extends boolean = false>({
 			height="auto"
 			className="w-full object-cover"
 			controls={controls}
+			volume={0}
+			muted={true}
+			playing={true}
 		/>
-	);
-};
+	)
+}
 
-export default DynamicVideoPlayer;
+export default DynamicVideoPlayer
