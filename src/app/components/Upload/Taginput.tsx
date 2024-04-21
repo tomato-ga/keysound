43653c6487ba486244902cae3013bd50f41789cc
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-04-21 23:42:05 +0900

    タグサジェスト修正した

--- src/app/components/Upload/Taginput.tsx
+++ src/app/components/Upload/Taginput.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState, useTransition } from 'react'
 import { useDebounce } from './useDebounce'
 import { getSuggestedTags } from '@/app/actions/getSuggestedTags/getSuggestedTags'
+import { PostFormData } from '../../../../types'
 
 interface TagInputProps {
-	tags: string[]
-	tagInput: string
-	onTagInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-	onAddTags: (tag?: string) => void
-	onRemoveTag: (index: number) => void
+	postData: PostFormData
+	setPostData: React.Dispatch<React.SetStateAction<PostFormData>>
 }
 
-const TagInput: React.FC<TagInputProps> = ({ tags, tagInput, onTagInputChange, onAddTags, onRemoveTag }) => {
+const TagInput: React.FC<TagInputProps> = ({ postData, setPostData }) => {
+	const [tagInput, setTagInput] = useState<string>('')
 	const [suggestedTags, setSuggestedTags] = useState<string[]>([])
 	const [selectedSuggestionIndex, setSelectedSuggestionIndex] = useState<number>(-1)
 	const [isPending, startTransition] = useTransition()
@@ -28,24 +27,22 @@ const TagInput: React.FC<TagInputProps> = ({ tags, tagInput, onTagInputChange, o
 	}, [debouncedTagInput])
 
 	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-		console.log(`Key Pressed: ${e.key}, Selected Index: ${selectedSuggestionIndex}`) // デバッグ用ログを出力
+		console.log(`Key pressed: ${e.key}, Selected index: ${selectedSuggestionIndex}`)
 		if (e.key === 'Enter') {
 			e.preventDefault()
 			if (selectedSuggestionIndex >= 0) {
-				console.log(`Adding suggested tag: ${suggestedTags[selectedSuggestionIndex]}`) // 選択されたサジェストタグをログに出力
-				onAddTags(suggestedTags[selectedSuggestionIndex])
-				onTagInputChange({ target: { value: '' } } as React.ChangeEvent<HTMLInputElement>)
-				setSelectedSuggestionIndex(-1)
-				setSuggestedTags([]) // サジェストリストをクリア
+				console.log('suggestedTags', suggestedTags)
+				const selectedTag = suggestedTags[selectedSuggestionIndex]
+				handleAddTags(selectedTag)
 			} else {
-				console.log(`Adding input tag: ${tagInput}`) // 入力されたタグをログに出力
-				onAddTags(tagInput)
-				onTagInputChange({ target: { value: '' } } as React.ChangeEvent<HTMLInputElement>)
+				handleAddTags(tagInput.trim())
 			}
 		} else if (e.key === 'ArrowDown') {
+			console.log(`Key pressed: ${e.key}, Selected index: ${selectedSuggestionIndex}`)
 			e.preventDefault()
 			setSelectedSuggestionIndex((prevIndex) => Math.min(prevIndex + 1, suggestedTags.length - 1))
 		} else if (e.key === 'ArrowUp') {
+			console.log(`Key pressed: ${e.key}, Selected index: ${selectedSuggestionIndex}`)
 			e.preventDefault()
 			setSelectedSuggestionIndex((prevIndex) => Math.max(prevIndex - 1, -1))
 		} else if (e.key === 'Tab') {
@@ -58,11 +55,34 @@ const TagInput: React.FC<TagInputProps> = ({ tags, tagInput, onTagInputChange, o
 		}
 	}
 
-	const handleSuggestedTagClick = (tag: string) => {
-		onAddTags(tag)
-		onTagInputChange({ target: { value: '' } } as React.ChangeEvent<HTMLInputElement>)
-		setSelectedSuggestionIndex(-1)
-		setSuggestedTags([]) // サジェストを選択した後、サジェストリストをクリアする
+	const handleAddTags = (tag?: string) => {
+		if ((tag && tag.trim()) || tagInput.trim()) {
+			const newTags = tag
+				? [tag]
+				: tagInput
+						.split(',')
+						.map((t) => t.trim())
+						.filter((t) => t !== '')
+			setPostData((prevState) => ({
+				...prevState,
+				tags: [...(prevState.tags || []), ...newTags]
+			}))
+			setTagInput('')
+			setSelectedSuggestionIndex(-1)
+			setSuggestedTags([])
+		}
+	}
+
+	const handleRemoveTag = (index: number) => {
+		setPostData((prevState) => ({
+			...prevState,
+			tags: prevState.tags?.filter((_, i) => i !== index) || []
+		}))
+	}
+
+	const handleSuggestedTagClick = (tag: string, index: number) => {
+		handleAddTags(tag)
+		setSelectedSuggestionIndex(index)
 	}
 
 	return (
@@ -78,7 +98,7 @@ const TagInput: React.FC<TagInputProps> = ({ tags, tagInput, onTagInputChange, o
 								className={`px-4 py-2 hover:bg-gray-100 cursor-pointer ${
 									index === selectedSuggestionIndex ? 'bg-gray-200' : ''
 								}`}
-								onClick={() => handleSuggestedTagClick(tag)}
+								onClick={() => handleSuggestedTagClick(tag, index)}
 							>
 								{tag}
 							</li>
@@ -93,23 +113,23 @@ const TagInput: React.FC<TagInputProps> = ({ tags, tagInput, onTagInputChange, o
 					placeholder="複数のタグを入力する場合は[タグを追加]ボタンを押すか、Enterキーを押してください"
 					className="w-full bg-gray-50 border border-gray-400 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-500"
 					value={tagInput}
-					onChange={onTagInputChange}
+					onChange={(e) => setTagInput(e.target.value)}
 					onKeyDown={handleKeyDown}
 				/>
 				<button
 					className="mt-4 bg-gray-200 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded"
 					onClick={(e) => {
 						e.preventDefault()
-						onAddTags()
+						handleAddTags()
 					}}
 				>
 					タグを追加
 				</button>
 				<div className="flex flex-wrap mt-4">
-					{tags.map((tag, index) => (
+					{postData.tags?.map((tag, index) => (
 						<div key={index} className="bg-gray-200 rounded-full px-3 py-1 mr-2 mb-2 flex items-center">
 							<span>{tag}</span>
-							<button className="ml-2 text-gray-600 hover:text-gray-800" onClick={() => onRemoveTag(index)}>
+							<button className="ml-2 text-gray-600 hover:text-gray-800" onClick={() => handleRemoveTag(index)}>
 								×
 							</button>
 						</div>
