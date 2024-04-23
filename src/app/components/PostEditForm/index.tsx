1ac8dbba72d908697506479524068cb8f3b71c4c
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-04-23 23:05:07 +0900

    パーツとタグのインプットをどうするか考え中

--- src/app/components/PostEditForm/index.tsx
+++ src/app/components/PostEditForm/index.tsx
@@ -1,19 +1,77 @@
+// /Users/ore/Documents/GitHub/keysound/src/app/components/PostEditForm/index.tsx
 'use client'
-
 import { prisma } from '@/app/lib/prisma'
-import { Post } from '../../../../types'
+import { Post, PostFormData, PostPart, PostEditFormData, UpdateTags, UpdateParts } from '../../../../types'
+import TitleInput from '../Upload/TitleInput'
+import DescriptionInput from '../Upload/DescriptionInput'
+import PartsInput from '../Upload/PartsInput'
+import TagInput from '../Upload/Taginput'
+import FileUploadButton from '../Upload/FileUploadButton'
+import SaveButton from '../Upload/SaveButton'
+import PreviewSection from '../Upload/PreviewSection'
+import { useState, ChangeEvent } from 'react'
 
-type PostWithoutUser = Omit<Post, 'user'>
-interface PostEditFormProps {
-	post: PostWithoutUser
-}
+const PostEditForm: React.FC<{ post: PostEditFormData }> = ({ post }) => {
+	const [postData, setPostData] = useState<PostEditFormData>({
+		id: post.id,
+		title: post.title,
+		description: post.description,
+		imageUrl: post.imageUrl,
+		videoUrl: post.videoUrl ?? '',
+		createdat: post.createdat,
+		updatedat: post.updatedat,
+		part: post.part,
+		tags: post.tags
+	})
 
-const PostEditForm: React.FC<PostEditFormProps> = ({ post }) => {
-	console.log('post', post)
 
-	// TODO Input系のコンポーネント呼び出し
+	console.log('PostEditForm State', postData)
 
-	return <>はろーコンポーネント修正中だよ</>
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		try {
+			// TODO: 更新リクエストを送信する処理を実装
+			console.log('Updated postData data:', postData)
+		} catch (error) {
+			console.error('Error updating post:', error)
+		}
+	}
+
+	const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setPostData((prevData) => ({ ...prevData, title: e.target.value }))
+	}
+
+	const handleDescriptionChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+		setPostData((prevData) => ({ ...prevData, description: e.target.value }))
+	}
+
+	const handlePartsChange = (parts: UpdateParts | null) => {
+		setPostData((prevData) => ({ ...prevData, part: parts }))
+	}
+
+	const handleTagsChange = (tags: UpdateTags[]) => {
+		setPostData((prevData) => ({ ...prevData, tags: tags.map((tag) => tag.name) }))
+	}
+
+	return (
+		<div className="bg-white text-black min-h-screen">
+			<div className="container mx-auto px-4 py-8">
+				<div className="bg-white">
+					<h1 className="text-4xl font-bold mb-8">投稿を編集</h1>
+					<form onSubmit={handleSubmit}>
+						<TitleInput title={postData.title} onTitleChange={handleTitleChange} />
+						<DescriptionInput description={postData.description} onDescriptionChange={handleDescriptionChange} />
+						<PartsInput parts={postData.part} onPartsChange={handlePartsChange} />
+						<TagInput tags={postData.tags.map((tag) => ({ id: '', name: tag }))} onTagsChange={handleTagsChange} />
+						{/* TODO: ファイルを削除できるようにする memo: 上げ直したりする可能性 */}
+						{/* <FileUploadButton /> */}
+						<SaveButton type="submit" />
+					</form>
+					{/* <PreviewSection /> */}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default PostEditForm
