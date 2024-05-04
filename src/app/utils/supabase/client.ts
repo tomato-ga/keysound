cc2335e1f6bebe5e74372de70ba2a1979d0811a6
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-05-04 13:06:43 +0900

    env整理

--- src/app/utils/supabase/client.ts
+++ src/app/utils/supabase/client.ts
@@ -1,9 +1,5 @@
 // /app/utils/supabase/client.ts
 
-import { createBrowserClient } from "@supabase/ssr";
+import { createBrowserClient } from '@supabase/ssr'
 
-export const createClient = () =>
-	createBrowserClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-	);
+export const createClient = () => createBrowserClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!)
