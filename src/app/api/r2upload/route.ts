d44cdf33b0e54e25c3e4bfb3199b872582a9ea05
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-05-04 23:05:28 +0900

    upload修正

--- src/app/api/r2upload/route.ts
+++ src/app/api/r2upload/route.ts
@@ -1,13 +1,8 @@
 // /Users/ore/Documents/GitHub/keysound/src/app/api/s3upload/route.ts
+
 import { NextRequest, NextResponse } from 'next/server'
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 require('dotenv').config()
-
-// export const config = {
-// 	api: {
-// 		bodyParser: false
-// 	}
-// }
 
 const s3Client = new S3Client({
 	region: 'auto',
@@ -24,30 +19,36 @@ export async function POST(request: NextRequest) {
 		const file = formData.get('file') as File | null
 
 		if (!file) {
+			console.log('ファイルがアップロードされていません。')
 			return NextResponse.json({ error: 'No file uploaded.' }, { status: 400 })
 		}
 
+		console.log('アップロードされたファイル:', file.name)
+
 		const now = new Date()
 		const formattedDate = now.toISOString().split('T')[0]
-		const encodedFileName = encodeURIComponent(file.name)
 		const fileBuffer = await file.arrayBuffer()
 		const fileStream = new Uint8Array(fileBuffer)
 
 		const uploadParams = {
 			Bucket: process.env.R2_BUCKET_NAME!,
-			Key: `uploads/${formattedDate}_${encodedFileName}`,
+			Key: `uploads/${formattedDate}_${file.name}`,
 			Body: fileStream
 		}
 
-		const uploadResult = await s3Client.send(new PutObjectCommand(uploadParams))
-		console.log('Upload Result:', uploadResult)
+		console.log('アップロードパラメータ:', uploadParams)
 
-		const url = `https://data.keyboard-sound.net/${uploadParams.Key}`
-		console.log(url)
+		const uploadResult = await s3Client.send(new PutObjectCommand(uploadParams))
+
+		console.log('アップロード結果:', uploadResult)
+
+		const url = `https://data.keyboard-sound.net/${decodeURIComponent(uploadParams.Key)}`
+
+		console.log('生成されたURL:', url)
 
 		return NextResponse.json({ url })
 	} catch (error) {
-		console.error('Error:', error)
+		console.error('エラーが発生しました:', error)
 		return NextResponse.json({ error: 'Server Error: Unable to process the request.' }, { status: 500 })
 	}
 }
