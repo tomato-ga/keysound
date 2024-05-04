83c5a5ecc755907cb5861c0e3eb34cda27e114c9
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-05-04 13:06:43 +0900

    env整理

--- src/app/utils/supabase/dbUserCheck.ts
+++ src/app/utils/supabase/dbUserCheck.ts
@@ -1,19 +1,16 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
 // Supabaseサービスの設定
-const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
-const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl: string = process.env.SUPABASE_URL || ''
+const supabaseAnonKey: string = process.env.SUPABASE_ANON_KEY || ''
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey)
 
 // 指定されたメールアドレスに一致するレコードがあるかどうかを確認する関数
 export async function checkUserExists(email: string): Promise<boolean> {
-	const { data, error } = await supabase
-		.from("User")
-		.select("email")
-		.eq("email", email);
+	const { data, error } = await supabase.from('User').select('email').eq('email', email)
 	if (error) {
 		// console.error('データ取得エラー:', error)
-		return false;
+		return false
 	}
-	return data.length > 0; // データが存在する場合はtrue、存在しない場合はfalse
+	return data.length > 0 // データが存在する場合はtrue、存在しない場合はfalse
 }
