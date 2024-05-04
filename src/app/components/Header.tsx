4211efafa8e6e57d6002e97c90202176e688edc6
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-05-04 21:55:19 +0900

    R2ドメイン変更

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -49,7 +49,7 @@ const Header: React.FC<HeaderProps> = ({ session }) => {
 			<div className="flex items-center justify-between w-full h-12 px-3">
 				<div className="ml-4">
 					<Link href="/">
-						<img src="/siteicon.png" className="h-12 w-auto" alt="みんなのキーボードサウンド" />
+						<img src="/siteicon.png" className="h-auto w-auto" alt="みんなのキーボードサウンド" />
 					</Link>
 				</div>
 				<div className="flex items-center justify-end flex-grow">
