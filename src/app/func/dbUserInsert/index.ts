39f58a09d39070c5455f2047af59f0d424e72757
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-05-05 13:43:14 +0900

    db user insert test

--- src/app/func/dbUserInsert/index.ts
+++ src/app/func/dbUserInsert/index.ts
@@ -1,28 +1,67 @@
 // dbUserInsert.ts
-export async function insertUserData(
-	name: string,
-	email: string,
-	image?: string,
-): Promise<{ userExists: boolean }> {
+
+import { prisma } from '@/app/lib/prisma' // prismaインスタンスのインポートを確認
+
+export async function insertUserData(name: string, email: string, image: string): Promise<{ userExists: boolean }> {
 	try {
-		const response = await fetch("/api/db/userSave", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ name, email, image }),
-		});
-		if (response.ok) {
-			const data = await response.json();
-			console.log(data.message);
-			return { userExists: false };
-		} else if (response.status === 400) {
-			return { userExists: true };
+		// ユーザーの存在チェック
+		const existingUser = await prisma.user.findUnique({
+			where: { email }
+		})
+
+		if (!existingUser) {
+			// ユーザーが存在しない場合は新規作成
+			const user = await prisma.user.create({
+				data: {
+					name,
+					email,
+					image,
+					profile: {
+						create: {
+							screenName: name // screenNameをnameと同じ値に設定
+						}
+					}
+				},
+				include: {
+					profile: true // 作成されたProfileも取得
+				}
+			})
+
+			return { userExists: false }
 		} else {
-			throw new Error("Failed to insert user data");
+			// ユーザーが既に存在する場合
+			return { userExists: true }
 		}
 	} catch (error) {
-		console.error("データ挿入時のエラー:", error);
-		throw error;
+		console.error('データ挿入時のエラー:', error)
+		throw error
 	}
 }
+
+// export async function insertUserData(
+// 	name: string,
+// 	email: string,
+// 	image?: string,
+// ): Promise<{ userExists: boolean }> {
+// 	try {
+// 		const response = await fetch("/api/db/userSave", {
+// 			method: "POST",
+// 			headers: {
+// 				"Content-Type": "application/json",
+// 			},
+// 			body: JSON.stringify({ name, email, image }),
+// 		});
+// 		if (response.ok) {
+// 			const data = await response.json();
+// 			console.log(data.message);
+// 			return { userExists: false };
+// 		} else if (response.status === 400) {
+// 			return { userExists: true };
+// 		} else {
+// 			throw new Error("Failed to insert user data");
+// 		}
+// 	} catch (error) {
+// 		console.error("データ挿入時のエラー:", error);
+// 		throw error;
+// 	}
+// }
