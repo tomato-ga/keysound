33a872f81e5e6f614d94806f9752e91799946c53
tomato-ga <servicedake@gmail.com>
2024-05-06 20:41:34 +0900

    新規user.profile.bio 作成するように修正

--- src/app/func/dbUserInsert/index.ts
+++ src/app/func/dbUserInsert/index.ts
@@ -3,12 +3,15 @@
 'use server'
 
 import { prisma } from '@/app/lib/prisma' // prismaインスタンスのインポートを確認
+import { User } from '../../../../types'
 
-export async function insertUserData(name: string, email: string, image: string): Promise<{ userExists: boolean }> {
+// TODO API経由でfetchしてキャッシュさせる
+export async function insertUserData(name: string, email: string, image: string) {
 	try {
 		// ユーザーの存在チェック
 		const existingUser = await prisma.user.findUnique({
-			where: { email }
+			where: { email },
+			include: { profile: true } // 関連する Profile も取得
 		})
 
 		if (!existingUser) {
@@ -20,50 +23,24 @@ export async function insertUserData(name: string, email: string, image: string)
 					image,
 					profile: {
 						create: {
-							screenName: name // screenNameをnameと同じ値に設定
+							bio: '',
+							screenName: name // screenName をname と同じ値に設定
+							// その他のデフォルト値を設定
 						}
 					}
 				},
 				include: {
-					profile: true // 作成されたProfileも取得
+					profile: true // 作成された Profile も取得
 				}
 			})
 
-			return { userExists: false }
+			return { userExists: false, user: user }
 		} else {
 			// ユーザーが既に存在する場合
-			return { userExists: true }
+			return { userExists: true, user: existingUser }
 		}
 	} catch (error) {
 		console.error('データ挿入時のエラー:', error)
 		throw error
 	}
 }
-
-// export async function insertUserData(
-// 	name: string,
-// 	email: string,
-// 	image?: string,
-// ): Promise<{ userExists: boolean }> {
-// 	try {
-// 		const response = await fetch("/api/db/userSave", {
-// 			method: "POST",
-// 			headers: {
-// 				"Content-Type": "application/json",
-// 			},
-// 			body: JSON.stringify({ name, email, image }),
-// 		});
-// 		if (response.ok) {
-// 			const data = await response.json();
-// 			console.log(data.message);
-// 			return { userExists: false };
-// 		} else if (response.status === 400) {
-// 			return { userExists: true };
-// 		} else {
-// 			throw new Error("Failed to insert user data");
-// 		}
-// 	} catch (error) {
-// 		console.error("データ挿入時のエラー:", error);
-// 		throw error;
-// 	}
-// }
