941e3688ea791722cff86391795417b92834b070
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-05-10 22:08:58 +0900

    動画アスペクト比を16:9へ修正

--- src/app/components/VideoPlayer/index.tsx
+++ src/app/components/VideoPlayer/index.tsx
@@ -15,28 +15,28 @@ const DynamicVideoPlayer = ({ videoUrl, controls, loop }: DynamicVideoPlayerProp
 		const handleResize = () => {
 			setIsMobile(window.innerWidth <= 768) // 画面幅が768px以下をモバイルとみなす
 		}
-
 		window.addEventListener('resize', handleResize)
 		handleResize() // 初回レンダリング時に画面幅を判定
-
 		return () => {
 			window.removeEventListener('resize', handleResize)
 		}
 	}, [])
 
 	return (
-		<ReactPlayer
-			url={videoUrl}
-			width="100%"
-			height="auto"
-			className="w-full object-cover"
-			controls={controls}
-			volume={0}
-			muted={true}
-			playing={!isMobile} // モバイルの場合はplayingをfalseに設定
-			loop={loop}
-			playsinline={true}
-		/>
+		<div className="relative w-full" style={{ aspectRatio: '16 / 9' }}>
+			<ReactPlayer
+				url={videoUrl}
+				width="100%"
+				height="100%"
+				className="absolute top-0 left-0"
+				controls={controls}
+				volume={0}
+				muted={true}
+				playing={!isMobile} // モバイルの場合はplayingをfalseに設定
+				loop={loop}
+				playsinline={true}
+			/>
+		</div>
 	)
 }
 
