7efcea924afe5bb962697978600ce0419ef2af2b
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-05-25 17:18:44 +0900

    空文字修正

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -26,25 +26,33 @@ const fetchParts = async (): Promise<PartsData> => {
 	const groupedByKeyCaps: GroupedParts = {}
 
 	getParts.forEach((part) => {
-		if (!groupedByCase[part.case]) {
+		if (part.case && !groupedByCase[part.case]) {
 			groupedByCase[part.case] = new Set()
 		}
-		groupedByCase[part.case].add(part.postId)
+		if (part.case) {
+			groupedByCase[part.case].add(part.postId)
+		}
 
-		if (!groupedBySwitches[part.switches]) {
+		if (part.switches && !groupedBySwitches[part.switches]) {
 			groupedBySwitches[part.switches] = new Set()
 		}
-		groupedBySwitches[part.switches].add(part.postId)
+		if (part.switches) {
+			groupedBySwitches[part.switches].add(part.postId)
+		}
 
-		if (!groupedByPlate[part.plate]) {
+		if (part.plate && !groupedByPlate[part.plate]) {
 			groupedByPlate[part.plate] = new Set()
 		}
-		groupedByPlate[part.plate].add(part.postId)
+		if (part.plate) {
+			groupedByPlate[part.plate].add(part.postId)
+		}
 
-		if (!groupedByKeyCaps[part.keyCaps]) {
+		if (part.keyCaps && !groupedByKeyCaps[part.keyCaps]) {
 			groupedByKeyCaps[part.keyCaps] = new Set()
 		}
-		groupedByKeyCaps[part.keyCaps].add(part.postId)
+		if (part.keyCaps) {
+			groupedByKeyCaps[part.keyCaps].add(part.postId)
+		}
 	})
 
 	return {
@@ -65,14 +73,16 @@ const Sidebar = async () => {
 					<h4 className="text-xl font-bold mb-3">ケース</h4>
 					{Object.keys(groupedByCase).length > 0 ? (
 						<div className="flex flex-wrap">
-							{Object.keys(groupedByCase).map((caseName) =>
-								Array.from(groupedByCase[caseName]).map((postId) => (
-									<Link key={postId} href={`/post/${postId}`}>
-										<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
-											{caseName}
-										</Button>
-									</Link>
-								))
+							{Object.keys(groupedByCase).map(
+								(caseName) =>
+									caseName &&
+									Array.from(groupedByCase[caseName]).map((postId) => (
+										<Link key={postId} href={`/post/${postId}`}>
+											<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
+												{caseName}
+											</Button>
+										</Link>
+									))
 							)}
 						</div>
 					) : (
@@ -83,14 +93,16 @@ const Sidebar = async () => {
 					<h4 className="text-xl font-bold mb-3">スイッチ</h4>
 					{Object.keys(groupedBySwitches).length > 0 ? (
 						<div className="flex flex-wrap">
-							{Object.keys(groupedBySwitches).map((switchName) =>
-								Array.from(groupedBySwitches[switchName]).map((postId) => (
-									<Link key={postId} href={`/post/${postId}`}>
-										<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
-											{switchName}
-										</Button>
-									</Link>
-								))
+							{Object.keys(groupedBySwitches).map(
+								(switchName) =>
+									switchName &&
+									Array.from(groupedBySwitches[switchName]).map((postId) => (
+										<Link key={postId} href={`/post/${postId}`}>
+											<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
+												{switchName}
+											</Button>
+										</Link>
+									))
 							)}
 						</div>
 					) : (
@@ -101,14 +113,16 @@ const Sidebar = async () => {
 					<h4 className="text-xl font-bold mb-3">プレート</h4>
 					{Object.keys(groupedByPlate).length > 0 ? (
 						<div className="flex flex-wrap">
-							{Object.keys(groupedByPlate).map((plateName) =>
-								Array.from(groupedByPlate[plateName]).map((postId) => (
-									<Link key={postId} href={`/post/${postId}`}>
-										<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
-											{plateName}
-										</Button>
-									</Link>
-								))
+							{Object.keys(groupedByPlate).map(
+								(plateName) =>
+									plateName &&
+									Array.from(groupedByPlate[plateName]).map((postId) => (
+										<Link key={postId} href={`/post/${postId}`}>
+											<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
+												{plateName}
+											</Button>
+										</Link>
+									))
 							)}
 						</div>
 					) : (
@@ -119,14 +133,16 @@ const Sidebar = async () => {
 					<h4 className="text-xl font-bold mb-3">キーキャップ</h4>
 					{Object.keys(groupedByKeyCaps).length > 0 ? (
 						<div className="flex flex-wrap">
-							{Object.keys(groupedByKeyCaps).map((keyCapsName) =>
-								Array.from(groupedByKeyCaps[keyCapsName]).map((postId) => (
-									<Link key={postId} href={`/post/${postId}`}>
-										<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
-											{keyCapsName}
-										</Button>
-									</Link>
-								))
+							{Object.keys(groupedByKeyCaps).map(
+								(keyCapsName) =>
+									keyCapsName &&
+									Array.from(groupedByKeyCaps[keyCapsName]).map((postId) => (
+										<Link key={postId} href={`/post/${postId}`}>
+											<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
+												{keyCapsName}
+											</Button>
+										</Link>
+									))
 							)}
 						</div>
 					) : (
