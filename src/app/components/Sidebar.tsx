2a6fed6cf05dc6080399ce110d970deaf6a72632
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-05-25 17:14:36 +0900

    サイドバー修正

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,46 +1,70 @@
+// components/Sidebar.tsx
 import React from 'react'
-import { prisma } from '../lib/prisma'
 import Link from 'next/link'
+import { prisma } from '../lib/prisma'
 import { Button } from '@/components/ui/button'
 
-const Sidebar: React.FC = async () => {
+interface GroupedParts {
+	[key: string]: Set<string>
+}
+
+interface PartsData {
+	groupedByCase: GroupedParts
+	groupedBySwitches: GroupedParts
+	groupedByPlate: GroupedParts
+	groupedByKeyCaps: GroupedParts
+}
+
+const fetchParts = async (): Promise<PartsData> => {
 	const getParts = await prisma.part.findMany({
 		take: 10
 	})
 
-	console.log('getParts', getParts)
-
-	// グループ化処理
-	const groupedByCase: { [key: string]: Set<string> } = {}
-	const groupedBySwitches: { [key: string]: Set<string> } = {}
-	const groupedByPlate: { [key: string]: Set<string> } = {}
-	const groupedByKeyCaps: { [key: string]: Set<string> } = {}
+	const groupedByCase: GroupedParts = {}
+	const groupedBySwitches: GroupedParts = {}
+	const groupedByPlate: GroupedParts = {}
+	const groupedByKeyCaps: GroupedParts = {}
 
 	getParts.forEach((part) => {
-		// ケースのグループ化
-		if (!groupedByCase[part.case]) {
+		if (part.case && !groupedByCase[part.case]) {
 			groupedByCase[part.case] = new Set()
 		}
-		groupedByCase[part.case].add(part.postId)
+		if (part.case) {
+			groupedByCase[part.case].add(part.postId)
+		}
 
-		// スイッチのグループ化
-		if (!groupedBySwitches[part.switches]) {
+		if (part.switches && !groupedBySwitches[part.switches]) {
 			groupedBySwitches[part.switches] = new Set()
 		}
-		groupedBySwitches[part.switches].add(part.postId)
+		if (part.switches) {
+			groupedBySwitches[part.switches].add(part.postId)
+		}
 
-		// プレートのグループ化
-		if (!groupedByPlate[part.plate]) {
+		if (part.plate && !groupedByPlate[part.plate]) {
 			groupedByPlate[part.plate] = new Set()
 		}
-		groupedByPlate[part.plate].add(part.postId)
+		if (part.plate) {
+			groupedByPlate[part.plate].add(part.postId)
+		}
 
-		// キーキャップのグループ化
-		if (!groupedByKeyCaps[part.keyCaps]) {
+		if (part.keyCaps && !groupedByKeyCaps[part.keyCaps]) {
 			groupedByKeyCaps[part.keyCaps] = new Set()
 		}
-		groupedByKeyCaps[part.keyCaps].add(part.postId)
+		if (part.keyCaps) {
+			groupedByKeyCaps[part.keyCaps].add(part.postId)
+		}
 	})
+
+	return {
+		groupedByCase,
+		groupedBySwitches,
+		groupedByPlate,
+		groupedByKeyCaps
+	}
+}
+
+const Sidebar = async () => {
+	const { groupedByCase, groupedBySwitches, groupedByPlate, groupedByKeyCaps } = await fetchParts()
 
 	return (
 		<div className="bg-white p-6 order-2 md:order-1 transform w-full md:w-1/3 lg:w-1/4 rounded-lg shadow-lg">
@@ -49,12 +73,16 @@ const Sidebar: React.FC = async () => {
 					<h4 className="text-xl font-bold mb-3">ケース</h4>
 					{Object.keys(groupedByCase).length > 0 ? (
 						<div className="flex flex-wrap">
-							{Object.keys(groupedByCase).map((caseName) =>
-								Array.from(groupedByCase[caseName]).map((postId) => (
-									<Link key={postId} href={`/post/${postId}`}>
-										<Button className="m-2">{caseName}</Button>
-									</Link>
-								))
+							{Object.keys(groupedByCase).map(
+								(caseName) =>
+									caseName &&
+									Array.from(groupedByCase[caseName]).map((postId) => (
+										<Link key={postId} href={`/post/${postId}`}>
+											<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
+												{caseName}
+											</Button>
+										</Link>
+									))
 							)}
 						</div>
 					) : (
@@ -65,12 +93,16 @@ const Sidebar: React.FC = async () => {
 					<h4 className="text-xl font-bold mb-3">スイッチ</h4>
 					{Object.keys(groupedBySwitches).length > 0 ? (
 						<div className="flex flex-wrap">
-							{Object.keys(groupedBySwitches).map((switchName) =>
-								Array.from(groupedBySwitches[switchName]).map((postId) => (
-									<Link key={postId} href={`/post/${postId}`}>
-										<Button className="m-2">{switchName}</Button>
-									</Link>
-								))
+							{Object.keys(groupedBySwitches).map(
+								(switchName) =>
+									switchName &&
+									Array.from(groupedBySwitches[switchName]).map((postId) => (
+										<Link key={postId} href={`/post/${postId}`}>
+											<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
+												{switchName}
+											</Button>
+										</Link>
+									))
 							)}
 						</div>
 					) : (
@@ -81,12 +113,16 @@ const Sidebar: React.FC = async () => {
 					<h4 className="text-xl font-bold mb-3">プレート</h4>
 					{Object.keys(groupedByPlate).length > 0 ? (
 						<div className="flex flex-wrap">
-							{Object.keys(groupedByPlate).map((plateName) =>
-								Array.from(groupedByPlate[plateName]).map((postId) => (
-									<Link key={postId} href={`/post/${postId}`}>
-										<Button className="m-2">{plateName}</Button>
-									</Link>
-								))
+							{Object.keys(groupedByPlate).map(
+								(plateName) =>
+									plateName &&
+									Array.from(groupedByPlate[plateName]).map((postId) => (
+										<Link key={postId} href={`/post/${postId}`}>
+											<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
+												{plateName}
+											</Button>
+										</Link>
+									))
 							)}
 						</div>
 					) : (
@@ -97,12 +133,16 @@ const Sidebar: React.FC = async () => {
 					<h4 className="text-xl font-bold mb-3">キーキャップ</h4>
 					{Object.keys(groupedByKeyCaps).length > 0 ? (
 						<div className="flex flex-wrap">
-							{Object.keys(groupedByKeyCaps).map((keyCapsName) =>
-								Array.from(groupedByKeyCaps[keyCapsName]).map((postId) => (
-									<Link key={postId} href={`/post/${postId}`}>
-										<Button className="m-2">{keyCapsName}</Button>
-									</Link>
-								))
+							{Object.keys(groupedByKeyCaps).map(
+								(keyCapsName) =>
+									keyCapsName &&
+									Array.from(groupedByKeyCaps[keyCapsName]).map((postId) => (
+										<Link key={postId} href={`/post/${postId}`}>
+											<Button className="m-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full">
+												{keyCapsName}
+											</Button>
+										</Link>
+									))
 							)}
 						</div>
 					) : (
