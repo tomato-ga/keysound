bcb268a84389d34cdf8c2f9918fa852b91e5bf9a
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-05-25 17:40:44 +0900

    css調整

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -1,4 +1,4 @@
-// components/ui/Button.tsx
+// components/ui/button.tsx
 import React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
