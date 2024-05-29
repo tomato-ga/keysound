92b720b2cd9562e0572ef961ed4d4da1e373a279
tomato-ga <servicedake@gmail.com>
2024-05-29 14:30:18 +0900

    metadata修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,32 @@ const noto = Noto_Sans_JP({ subsets: ['latin'], weight: ['500'] })
 
 export const metadata: Metadata = {
 	title: 'keyboard sound',
-	description: 'キーボードの「音」に特化したサイト'
+	description: 'キーボードサウンドは、キーボードの「打鍵音」に特化した、ニッチな場所を目指しています。 自分のお気に入りキーボードの打鍵音を投稿したり、他の人の投稿を探索したりできるようになる予定です。 ぜひみなさんのキーボードの音を聞かせてください。',
+	openGraph: {
+		title: 'keyboard sound',
+		description: 'キーボードサウンドは、キーボードの「打鍵音」に特化した、ニッチな場所を目指しています。 自分のお気に入りキーボードの打鍵音を投稿したり、他の人の投稿を探索したりできるようになる予定です。 ぜひみなさんのキーボードの音を聞かせてください。',
+		images: [
+			{
+				url: '/opengraph-image.jpg', // ここで画像のパスを指定
+				width: 1280,
+				height: 720,
+				alt: 'Open Graph Image'
+			}
+		]
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'keyboard sound',
+		description: 'キーボードサウンドは、キーボードの「打鍵音」に特化した、ニッチな場所を目指しています。 自分のお気に入りキーボードの打鍵音を投稿したり、他の人の投稿を探索したりできるようになる予定です。 ぜひみなさんのキーボードの音を聞かせてください。',
+		images: [
+			{
+				url: '/twitter-image.jpg', // ここで画像のパスを指定
+				width: 1280,
+				height: 720,
+				alt: 'Twitter Image'
+			}
+		]
+	}
 }
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
