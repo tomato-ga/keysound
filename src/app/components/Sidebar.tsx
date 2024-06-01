9f96cefe0f6f7218cf1d6583573dbcff72db2c3b
tomato <52771764+tomato-ga@users.noreply.github.com>
2024-06-01 19:10:30 +0900

    サイドバー左側に配置

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -66,7 +66,7 @@ const Sidebar = async () => {
 	const { groupedByCase, groupedBySwitches, groupedByPlate, groupedByKeyCaps } = await fetchParts()
 
 	return (
-		<div className="bg-white p-6 order-2 md:order-1 transform w-full md:w-80 border-r mt-2">
+		<div className="bg-white p-6 transform w-full md:w-80 border-r mt-2">
 			<div className="text-black space-y-6">
 				<div>
 					<h4 className="text-xl font-bold mb-3">ケース</h4>
