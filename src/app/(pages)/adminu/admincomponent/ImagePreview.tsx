678d34288f8f3539423283f957a5ea663d00ed69
tomato-ga <servicedake@gmail.com>
2024-06-11 16:35:41 +0900

    ファイル名追加

--- src/app/(pages)/adminu/admincomponent/ImagePreview.tsx
+++ src/app/(pages)/adminu/admincomponent/ImagePreview.tsx
@@ -27,22 +27,32 @@ const ImagePreview: React.FC<ImagePreviewProps> = ({ imageUrls, thumbUrl, onDele
 		}
 	}
 
+	const getFileName = (url: string) => {
+		return url.split('/').pop() || 'Unknown'
+	}
+
 	return (
-		<div className="image-preview-container">
+		<div className="flex flex-col gap-4">
 			{thumbUrl && (
-				<div className="image-preview-item">
-					<img src={thumbUrl} alt="Thumbnail Preview" className="image-preview" />
-					<button onClick={() => handleDelete(thumbUrl)} className="image-delete-button">
-						削除
-					</button>
+				<div className="flex flex-col items-center">
+					<img src={thumbUrl} alt="Thumbnail Preview" className="max-w-full h-auto" />
+					<div className="flex justify-between items-center w-full mt-2">
+						<span className="mr-4">{getFileName(thumbUrl)}</span>
+						<button onClick={() => handleDelete(thumbUrl)} className="bg-red-500 text-white px-4 py-2 rounded">
+							削除
+						</button>
+					</div>
 				</div>
 			)}
 			{imageUrls.map((url, index) => (
-				<div key={index} className="image-preview-item">
-					<img src={url} alt={`Preview ${index + 1}`} className="image-preview" />
-					<button onClick={() => handleDelete(url)} className="image-delete-button">
-						削除
-					</button>
+				<div key={index} className="flex flex-col items-center">
+					<img src={url} alt={`Preview ${index + 1}`} className="max-w-full h-auto" />
+					<div className="flex justify-between items-center w-full mt-2">
+						<span className="mr-4">{getFileName(url)}</span>
+						<button onClick={() => handleDelete(url)} className="bg-red-500 text-white px-4 py-2 rounded">
+							削除
+						</button>
+					</div>
 				</div>
 			))}
 		</div>
